f74f36058d06a73da7fff14ee46f3b1237b083f1
alannn118 <linnnda75@gmail.com>
2025-05-30 20:33:14 +0800

    Delete beLiver_backend directory

--- beLiver_backend/app/crud/crud_user.py
+++
@@ -1,6 +0,0 @@
-# 使用者table crud
-from sqlalchemy.orm import Session
-from models import User
-
-def get_user_by_email(db: Session, email: str) -> User | None:
-    return db.query(User).filter(User.email == email).first()
